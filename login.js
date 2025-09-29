13b09367e0cba0b4a368c23a89853a15289e579b
Guilherme76565 <guilherme.ma5@puccampinas.edu.br>
2025-09-29 00:11:24 -0300

    css e correção do js

---
+++ login.js
@@ -0,0 +1,33 @@
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+const form = document.getElementById('loginForm');
+const globalError = document.getElementById('globalError');
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let email = emailInput.value.trim();
+    let password = passwordInput.value;
+
+    if (email === "") {
+        globalError.textContent = "E-mail ou senha incorretos ou inválidos.";
+        return;
+    }
+
+    if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+        globalError.textContent = "E-mail ou senha incorretos ou inválidos.";
+        return;
+    }
+
+    if (password === "") {
+        globalError.textContent = "E-mail ou senha incorretos ou inválidos.";
+        return;
+    }
+
+    globalError.textContent = "";
+    alert("Login realizado com sucesso!");
+    emailInput.value = "";
+    passwordInput.value = "";
+});
