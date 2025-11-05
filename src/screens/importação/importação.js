208f5c1c025065824a44188d74a90ab0bae22dba
luanbpt2 <edson.lr@puccampinas.edu.br>
2025-11-05 08:36:20 -0300

    importação

---
+++ src/screens/importação/importação.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+
+const router = express.Router();
+
+const upload = multer({ dest: 'uploads/' }); 
+
+router.post('/alunos/importar', upload.single('arquivo_alunos'), (req, res) => {
+    
+    if (!req.file) {
+        return res.status(400).send({ message: 'Nenhum arquivo foi enviado.' });
+    }
+
+    console.log('Arquivo recebido pelo backend:', req.file);
+    
+    res.status(200).send({ message: 'Arquivo recebido com sucesso e pronto para processar!' });
+});
+
+module.exports = router;
